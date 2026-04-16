cafbf2af3dd421ee51a1f24a48785fcdf3c2a464
a_abramov <atgunnn@gmail.com>
2026-04-16 18:10:09 +0200

    push value/cb in a queue only if cb/value queue is empty

--- include/mpmc_basic.hpp
+++ include/mpmc_basic.hpp
@@ -16,17 +16,13 @@ public:
         std::unique_lock lock{mutex_};
         if (isClosed_) return false;
 
-        valuesQueue_.push(std::move(value));
         if (!callbacksQueue_.empty()) {
             auto execCb = std::move(callbacksQueue_.front());
             callbacksQueue_.pop();
-
-            auto val = std::move(valuesQueue_.front());
-            valuesQueue_.pop();
-
             lock.unlock();
-
-            try { execCb(std::move(val)); } catch (...) {}
+            try { execCb(std::move(value)); } catch (...) {}
+        } else {
+            valuesQueue_.push(std::move(value));
         }
         return true;
     }
@@ -34,12 +30,8 @@ public:
     using DequeueCallback = std::function<void(std::optional<T>)>;
     void Dequeue(DequeueCallback cb) {
         std::unique_lock lock{mutex_};
-        callbacksQueue_.push(std::move(cb));
 
         if (!valuesQueue_.empty() || isClosed_) {
-            auto execCb = std::move(callbacksQueue_.front());
-            callbacksQueue_.pop();
-
             std::optional<T> val = std::nullopt;
             if (!valuesQueue_.empty()) {
                 val = std::move(valuesQueue_.front());
@@ -47,7 +39,9 @@ public:
             } 
             lock.unlock();
 
-            try { execCb(std::move(val)); } catch (...) {}
+            try { cb(std::move(val)); } catch (...) {}
+        } else {
+            callbacksQueue_.push(std::move(cb));
         } 
     }
     
